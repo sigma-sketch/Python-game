38641253625723ccbbd59fe330ce5c528cf251bf
Stephen <stephengreen434@gmail.com>
2026-02-23 22:30:22 +0000

    added clicker logic

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Your a dickhead")
+import pygame, keyboard
+
+points = 0
+upgrades = {"cps": 1, "click_power": 1}
+
+def main():
+    global points, upgrades
+    while True:
+        print(f"Points: {points}", end="\r")
+        if keyboard.is_pressed("space"):
+            points += upgrades["cps"]
