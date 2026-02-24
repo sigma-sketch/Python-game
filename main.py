5f0e692107fbbce290f3a9348e69c054c4070ca8
stephen <stephengreen434@gmail.com>
2026-02-24 14:45:25 +0000

    Added full working code so the clicker works now

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import pygame, keyboard
+#import pygame
+import keyboard
+import time
 
 points = 0
 upgrades = {"cps": 1, "click_power": 1}
 
+def on_release(event):
+    global points, upgrades
+    if event.name == "space":
+        points += upgrades["cps"]
+
+
 def main():
     global points, upgrades
+    keyboard.on_release(on_release)
     while True:
-        print(f"Points: {points}", end="\r")
-        if keyboard.is_pressed("space"):
-            points += upgrades["cps"]
+        print(f"\033[2K\033[1APoints: {points}\n\033[2KUpgrades: {upgrades}", end="\r", flush=True)
+        time.sleep(0.1)
+if __name__ == "__main__":
+    main()
